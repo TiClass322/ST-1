da528e524fec673ea4983873a6289bf795f193a6
TiClass322 <dana.kopilov@gmail.com>
2026-03-01 12:13:09 +0300

    READY

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,7 +4,6 @@
 #include <cstdint>
 #include "alg.h"
 
-
 TEST(st1, sumPrime1) {
   uint64_t res = sumPrime(2000000);
   uint64_t expected = 142913828922;
@@ -14,4 +13,73 @@ TEST(st1, sumPrime2) {
   uint64_t res = sumPrime(10);
   uint64_t expected = 17;
   EXPECT_EQ(expected, res);
+}
+
+TEST(CheckPrimeTest, SmallNonPrimes) {
+    EXPECT_FALSE(checkPrime(0));
+    EXPECT_FALSE(checkPrime(1));
+}
+
+TEST(CheckPrimeTest, SmallPrimes) {
+    EXPECT_TRUE(checkPrime(2));
+    EXPECT_TRUE(checkPrime(3));
+    EXPECT_TRUE(checkPrime(5));
+    EXPECT_TRUE(checkPrime(7));
+}
+
+TEST(CheckPrimeTest, CompositeNumbers) {
+    EXPECT_FALSE(checkPrime(4));
+    EXPECT_FALSE(checkPrime(9));
+    EXPECT_FALSE(checkPrime(15));
+    EXPECT_FALSE(checkPrime(25));
+}
+
+TEST(CheckPrimeTest, LargePrime) {
+    EXPECT_TRUE(checkPrime(982451653)); // большое простое число
+}
+
+TEST(NPrimeTest, FirstPrimes) {
+    EXPECT_EQ(nPrime(1), 2);
+    EXPECT_EQ(nPrime(2), 3);
+    EXPECT_EQ(nPrime(3), 5);
+    EXPECT_EQ(nPrime(4), 7);
+    EXPECT_EQ(nPrime(5), 11);
+}
+
+TEST(NPrimeTest, ZeroIndex) {
+    EXPECT_EQ(nPrime(0), 0);
+}
+
+TEST(NPrimeTest, LargerPrimes) {
+    EXPECT_EQ(nPrime(10), 29);
+    EXPECT_EQ(nPrime(25), 97);
+}
+
+TEST(NextPrimeTest, NextAfterSmallNumbers) {
+    EXPECT_EQ(nextPrime(1), 2);
+    EXPECT_EQ(nextPrime(2), 3);
+    EXPECT_EQ(nextPrime(4), 5);
+    EXPECT_EQ(nextPrime(10), 11);
+}
+
+TEST(NextPrimeTest, NextAfterPrimes) {
+    EXPECT_EQ(nextPrime(3), 5);
+    EXPECT_EQ(nextPrime(7), 11);
+    EXPECT_EQ(nextPrime(13), 17);
+}
+
+TEST(NextPrimeTest, MaxValue) {
+    EXPECT_EQ(nextPrime(UINT64_MAX), 0);
+}
+
+TEST(SumPrimeTest, SumBelowSmallBounds) {
+    EXPECT_EQ(sumPrime(2), 0);
+    EXPECT_EQ(sumPrime(3), 2);
+    EXPECT_EQ(sumPrime(6), 10);
+}
+
+TEST(SumPrimeTest, SumBelowLargerBounds) {
+    EXPECT_EQ(sumPrime(11), 17);
+    EXPECT_EQ(sumPrime(12), 28);
+    EXPECT_EQ(sumPrime(20), 77);
 }
